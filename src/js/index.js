ab1a091cfed631c82f6dc1d4aca6328a7903da39
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2023-05-14 01:42:43 -0300

    :sparkles: feat: adiciona funcionalide de voltar para o tema claro

--- src/js/index.js
+++ src/js/index.js
@@ -9,8 +9,9 @@
         - passo 5 - trocar a imagem do botão de alterar tema pra lua
     
     - objetivo 2 - quando clicar no botão de troca de tema, caso o body já tenha a classe modo-escuro, remover a classe pra mudar pro modo claro e mudar a imagem pro sol
-        - passo 6 - remover a classe do modo-escuro do body
-        - passo 7 - trocar a imagem do botão de alterar tema pra sol
+        - passo 6 - verificar se o body já tem a classe modo-escuro
+        - passo 7 - remover a classe do modo-escuro do body
+        - passo 8 - trocar a imagem do botão de alterar tema pra sol
 */
 
 //- objetivo 1 - quando clicar no botão de troca de tema, adicionar a classe modo-escuro no body pra que os estilos do modo escuro sejam aplicados e mudar a imagem pra lua
@@ -25,9 +26,22 @@ const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
 
 // - passo 3 - dar um jeito de identificar o clique do usuário no botão de troca de tema
 botaoAlterarTema.addEventListener("click", () => {
-  // - passo 4 - adicionar a classe modo-escuro no body
-  body.classList.add("modo-escuro")
+  //- objetivo 2 - quando clicar no botão de troca de tema, caso o body já tenha a classe modo-escuro, remover a classe pra mudar pro modo claro e mudar a imagem pro sol
 
-  // - passo 5 - trocar a imagem do botão de alterar tema pra lua
-  imagemBotaoTrocaDeTema.setAttribute("src", "./src/img/moon.png")
+  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro")
+
+  // - passo 6 - verificar se o body já tem a classe modo-escuro
+  if (modoEscuroEstaAtivo) {
+    // - passo 7 - remover a classe do modo-escuro do body
+    body.classList.remove("modo-escuro")
+
+    // - passo 8 - trocar a imagem do botão de alterar tema pra sol
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/img/sun.png")
+  } else {
+    // - passo 4 - adicionar a classe modo-escuro no body
+    body.classList.add("modo-escuro")
+
+    // - passo 5 - trocar a imagem do botão de alterar tema pra lua
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/img/moon.png")
+  }
 })
